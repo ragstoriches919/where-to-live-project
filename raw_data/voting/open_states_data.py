e2db26698f6e7fc53302eb3e9a47f60120ddf5a7
Raghu <ragstoriches919@gmail.com>
2022-07-06 10:57:28 -0400

    Playing around w new APIs

--- raw_data/voting/open_states_data.py
+++ raw_data/voting/open_states_data.py
@@ -1,5 +1,24 @@
 import pyopenstates
 import cfg
 
-pyopenstates.set_api_key(cfg.API_KEY_OPENSTATES)
-print(pyopenstates.get_metadata("CT"))
+# pyopenstates.set_api_key(cfg.API_KEY_OPENSTATES)
+# print(pyopenstates.get_metadata("CT"))
+
+
+import urllib
+from urllib.request import Request, urlopen
+
+
+# link =  r"https://datausa.io/api/data?drilldowns=Nation&measures=Population"
+link =  r"https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest"
+import pandas as pd
+import json
+req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+webpage = urlopen(req).read()
+webpage = json.loads(webpage.decode('utf8').replace("'", '"'))
+df = pd.json_normalize(webpage["data"])
+print(df)
+
+# f = urllib.request.urlopen(link)
+# myfile = f.read()
+# print(myfile)
